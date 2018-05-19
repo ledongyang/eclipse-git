e137a86ad79c653955a94e89c0d7a546c9117e19
Michael Yang <ledongyang@gmail.com>
2018-05-18 23:01:11 -0400

    Add setter and getter for name field

--- src/com/mikeyang/model/User.java
+++ src/com/mikeyang/model/User.java
@@ -1,5 +1,15 @@
 package com.mikeyang.model;
 
 public class User {
+    
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
 }
